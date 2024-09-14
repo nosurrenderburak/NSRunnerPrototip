35def99c98f161fb648b75076cfa07fc0e46b3b7
Burak Kardaş <burak.kardas@nosurrender.studio>
2024-09-14 19:50:38 +0300

    Added Utils Script

---
+++ NSRunnerPrototip/Assets/_Scripts/Utils/Utils.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+
+namespace NoSurrender
+{
+    public class Utils
+    {
+        public static bool IsPointerOverUIObject()
+        {
+            PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+            eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+            return results.Count > 0;
+        }
+    }
+}
