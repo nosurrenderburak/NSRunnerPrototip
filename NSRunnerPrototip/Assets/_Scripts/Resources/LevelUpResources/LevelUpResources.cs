e79d9efe347e22e89939ef4c4c866c833ae6038c
Burak Kardaş <burak.kardas@nosurrender.studio>
2024-09-14 11:58:35 +0300

    Added Level Up Resources Scriptable

---
+++ NSRunnerPrototip/Assets/_Scripts/Resources/LevelUpResources/LevelUpResources.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[CreateAssetMenu(fileName = "LevelUpResources", menuName = "Resources/LevelUpResources")]
+public class LevelUpResources : ScriptableObject
+{
+    [SerializeField] private List<LevelUpData> levelUpDatas = new ();
+    
+    public LevelUpData GetLevelUpData(int level) => levelUpDatas[level];
+}
+
+
+[Serializable]
+public struct LevelUpData
+{
+    [SerializeField] private Vector3 bulletScale;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float shootTime;
+    
+    public Vector3 BulletScale => bulletScale;
+    public float BulletSpeed => bulletSpeed;
+    public float ShootTime => shootTime;
+}
